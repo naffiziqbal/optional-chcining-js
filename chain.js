6f544cb95fdc75a7d0dac5f1d1950644f17317fd
naffiziqbal <f.nafiziqbal@gmail.com>
2022-08-27 13:13:05 +0600

    Hello

---
+++ chain.js
@@ -0,0 +1,11 @@
+const users = {
+    id: 1, name: "Nafiz", age: 30,
+    jobName: {
+        freelancer: "freelancer", baseSalary: 30000,
+        anotherJob : "Teacher", baseSalary: 2000,
+        category : "workfrom home", 
+    },
+
+}
+
+console.log(users.jobNaame?.baseSalary);
